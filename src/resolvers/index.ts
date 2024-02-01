ca701e627433f8b9a5736b7668c8d503f124bbb5
Jeff Soriano <soriano.jeffm@gmail.com>
2024-02-01 13:43:15 -0800

    Add graphql schemas

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,10 +1,20 @@
+// src/resolvers/resolvers.ts
 import { IResolvers } from '@graphql-tools/utils'
+import User from '../models/User' // Assuming you have a User model set up with Mongoose
 
 export const resolvers: IResolvers = {
   Query: {
-    // Implementation of query resolvers
+    async users() {
+      return await User.find({})
+    },
+    async user(_: any, { id }) {
+      return await User.findById(id)
+    },
   },
   Mutation: {
-    // Implementation of mutation resolvers
+    async createUser(_: any, { username, userType }) {
+      const newUser = new User({ username, userType })
+      return await newUser.save()
+    },
   },
 }
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,17 +1,19 @@
-import gql from 'graphql-tag'
+// src/schemas/typeDefs.ts
+import { gql } from 'graphql-tag'
 
-// Define your GraphQL types and schema
 export const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    userType: String!
+  }
+
   type Query {
-    // Define your queries here
-    // Example: users: [User]
+    users: [User]
+    user(id: ID!): User
   }
 
   type Mutation {
-    // Define your mutations here
-    // Example: addUser(username: String!): User
+    createUser(username: String!, userType: String!): User
   }
-
-  // Define your other types here
-  // Example: type User { id: ID!, username: String! }
 `
